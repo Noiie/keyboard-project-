39bad64412c82bd5ca8355111e2d2c1a45cc0ec7
noi <noieitan@hilma.tech>
2024-10-30 17:55:55 +0200

    rey

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import './App.css'
-import Textarea from './components/Textarea'
+import TextArea from './components/TextArea'
+import Keyboard from './components/Keyboard'
 
 function App() {
+  const keyboard = ["a", "b"];
+  console.log(keyboard)
+  const [text, setText] = useState("");
+
+  function write(character) {
+    setText(prev => prev + character);
+  }
+
   return (
     <>
-      <Textarea></Textarea>
+      <TextArea text={text} />
+      <Keyboard write={write} />
     </>
   )
 }
