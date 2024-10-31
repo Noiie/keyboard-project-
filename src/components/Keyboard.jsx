98bbfc666635ae558543cb2410bc1d258caafa78
RamAvni <ramavni@hilma.tech>
2024-10-31 10:01:42 +0200

    Better keyboard, space, delete

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -9,14 +9,24 @@ export default function Keyboard(props) {
     
     return (
         <>
-            {props.keyboardLayouts[props.layoutIndex].map((key) => (
-                <button onClick={() => props.write(key)}>{key}</button>
-            ))}
+            {props.keyboardLayouts[props.layoutIndex].map((key) => {
+                return <>
+                    <button onClick={() => props.write(key)}>{key}</button>
+                    {['=', ']', 'p', 'פ', '\\', '/'].includes(key) && <br />}
+                </>
+            })}
+
             <button onClick={props.swapLayoutIndex}>🌐</button>
             <button onClick={props.clear}>Clear</button>
             <button onClick={props.toUpperCase}>
                 Upper case
             </button>
+            <button onClick={props.space}>Space</button>
+            <button onClick={props.toLowerCase}>
+                Lower case
+            </button>
+            
+            <button onClick={props.deleteLast}>Delete</button>
         </>
     );
 }
