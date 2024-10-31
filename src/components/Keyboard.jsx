7684523d6bc3c7016946c8c87a71f382245e9722
noi <noieitan@hilma.tech>
2024-10-31 14:33:51 +0200

    khijij

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -18,11 +18,6 @@ export default function Keyboard(props) {
 
     return (
         <>
-<<<<<<< HEAD
-            {props.keyboardLayouts[props.layoutIndex].map((key) => (
-                <button onClick={() => props.write(key)}>{key}</button>
-            ))}
-=======
             {props.keyboardLayouts[props.layoutIndex].map((key) => {
                 return (
                     <>
@@ -32,29 +27,10 @@ export default function Keyboard(props) {
                 );
             })}
 
->>>>>>> 52398780c052bf901ffc86524d04f8bf21687f7f
             <button onClick={props.swapLayoutIndex}>🌐</button>
             <button onClick={props.clear}>Clear</button>
             {/* <button onClick={props.toUpperCase}>
                 Upper case
-<<<<<<< HEAD
-            </button>
-            
-            <br/><br/>
-            <button onClick={() => props.changeColor('red')}>red</button>
-            <button onClick={() => props.changeColor('blue')}>blue</button>
-            <button onClick={() => props.changeColor('green')}>green</button>
-            <button onClick={() => props.changeColor('black')}>black</button>
-
-
-            <br/><br/>
-            <button onClick={() => props.changeSize('20px')}>20px</button>
-            <button onClick={() => props.changeSize('30px')}>30px</button>
-            <button onClick={() => props.changeSize('40px')}>40px</button>
-
-
-
-=======
             </button> */}
             <button onClick={props.space}>Space</button>
             {/* <button onClick={props.toLowerCase}>
@@ -79,7 +55,6 @@ export default function Keyboard(props) {
             <button onClick={() => props.setStyle((prev) => ({ ...prev, fontFamily: "Garamond" }))}>Garamond</button>
 
             <button onClick={handleCapsLock}>Caps Lock</button>
->>>>>>> 52398780c052bf901ffc86524d04f8bf21687f7f
         </>
     );
 }
